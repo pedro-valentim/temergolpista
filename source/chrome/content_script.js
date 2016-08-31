c03f2b1ff9e5ebd6d39338c4b1ac2c1adf33d518
Guilmour <guilmour@alunos.utfpr.edu.br>
2016-08-31 10:36:09 -0300

    More adjetives 2

--- source/chrome/content_script.js
+++ source/chrome/content_script.js
@@ -26,11 +26,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bMICHEL TEMER\b/g, "TEMER GOLPISTA GOLPISTA");
-	v = v.replace(/\bMichel Temer\b/g, "Temer Golpista");
+	v = v.replace(/\bTEMER\b/g, "TEMER GOLPISTA");
+	v = v.replace(/\bTemer\b/g, "Temer Golpista");
 	v = v.replace(/\bPresidente Interino\b/g, "Presidente Sem Voto");
 	v = v.replace(/\bpresidente interino Michel Temer\b/g, "presidente sem voto");
-	v = v.replace(/\bMichel Miguel Elias Temer Lulia\b/g, "Temer Golpista");
 	v = v.replace(/^Michel Miguel Elias Temer Lulia$/g, "Presidente Golpista");
 	v = v.replace(/\bmichel miguel elias temer lulia\b/g, "presidente golpista");
 	v = v.replace(/\bPRESIDENTE MICHEL TEMER\b/g, "PRESIDENTE GOLPISTA SEM VOTO");
@@ -41,7 +40,6 @@ function handleText(textNode)
 	v = v.replace(/\bvice-presidente Michel Temer\b/g, "Vice-decorativo e golpista");
 	v = v.replace(/\bvice-presidente Michel Temer\b/g, "Vice-decorativo e golpista");
 	v = v.replace(/\bpresidente interino da República Federativa do Brasil\b/g, "presidente golpista sem voto da República Federativa do Brasil");
-	v = v.replace(/\bTemer\b/g, "Temer Golpista");
 
 	textNode.nodeValue = v;
 }
